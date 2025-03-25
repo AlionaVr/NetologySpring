4aaa74c6afb180091a43424f0e7000d491545c3c
AlionaVr <alonavershilo@yandex.by>
2025-03-25 23:32:46 +0300

    add HttpClient, handle Query parameters

---
+++ src/main/java/org/Client.java
@@ -0,0 +1,46 @@
+package org;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Client {
+    private final String HOST;
+    private final int PORT;
+    private final HttpClient httpClient;
+
+    public Client(String host, int port) {
+        this.HOST = host;
+        this.PORT = port;
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public void sendGetRequest(String path) throws IOException, InterruptedException {
+        String urlString = "http://" + HOST + ":" + PORT + path;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlString))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("GET Response: " + response);
+        System.out.println(response.body());
+    }
+
+    public void sendPostRequest(String path, String body) throws IOException, InterruptedException {
+        String urlString = "http://" + HOST + ":" + PORT + path;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlString))
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("POST Response Code: " + response.statusCode());
+        System.out.println(response.body());
+
+    }
+}
+
--- src/main/java/org/Request.java
+++ src/main/java/org/Request.java
@@ -4,19 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 
 public class Request {
-    // must be in form GET /path HTTP/1.1
     private final String method;
     private final String path;
+    private final Map<String, String> queryParams;
+    private final InputStream inputStream;
 
-
-    public Request(String method, String path, String version, Map<String, String> headers, InputStream inputStream) {
+    public Request(String method, String path, String version, Map<String, String> headers,
+                   InputStream inputStream, Map<String, String> queryParams) {
         this.method = method;
         this.path = path;
+        this.queryParams = queryParams;
+        this.inputStream = inputStream;
     }
 
     public static Request fromInputStream(InputStream inputStream) throws IOException {
@@ -29,16 +34,44 @@ public class Request {
         String[] parts = startLine.split(" ");
         if (parts.length != 3) {
             throw new IOException("Invalid request line: " + startLine);
-
         }
+
         String method = parts[0];
-        String path = parts[1];
+        String fullPath = parts[1];    // fullPath = '/search?key1=value1&k2=v2'
         String version = parts[2];
+
+        String path;
+        Map<String, String> queryParams;
+
+        int questionIndex = fullPath.indexOf('?');
+        if (questionIndex != -1) {
+            path = fullPath.substring(0, questionIndex);
+            String queryString = fullPath.substring(questionIndex + 1);
+            queryParams = parseQueryParams(queryString);
+        } else {
+            path = fullPath;
+            queryParams = new HashMap<>();
+        }
 
         Map<String, String> headers = parseHeaders(reader);
 
-        return new Request(method, path, version, headers, inputStream);
+        return new Request(method, path, version, headers, inputStream, queryParams);
     }
+
+    private static Map<String, String> parseQueryParams(String queryString) {
+        Map<String, String> queryParams = new HashMap<>();
+        String[] params = queryString.split("&");
+        for (String param : params) {
+            String[] keyValue = param.split("=", 2);
+            if (keyValue.length == 2) {
+                queryParams.put(URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8), URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8));
+            } else if (keyValue.length == 1) {
+                queryParams.put(URLDecoder.decode(keyValue[0], StandardCharsets.UTF_8), "");
+            }
+        }
+        return queryParams;
+    }
+
 
     private static Map<String, String> parseHeaders(BufferedReader reader) throws IOException {
         Map<String, String> headers = new HashMap<>();
@@ -61,6 +94,19 @@ public class Request {
 
     public String getMethod() {
         return method;
+    }
+
+    public String getQueryParam(String name) {
+        return queryParams.get(name);
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getBodyAsString() throws IOException {
+        byte[] bodyBytes = inputStream.readAllBytes();
+        return new String(bodyBytes, StandardCharsets.UTF_8);
     }
 }
 
